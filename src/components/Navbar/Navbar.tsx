2e38e5fd22e8233696bad026fbc410b08b3cc949
akopton <kopton.olek@gmail.com>
2023-11-19 14:49:26 +0100

    added navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { SignBtn } from "../SignBtn/SignBtn";
+import { SwitchThemeBtn } from "../SwitchThemeBtn/SwitchThemeBtn";
+import styles from "./navbar.module.css";
+import { ThemeContext } from "@/context/ThemeContext";
+
+export const Navbar = () => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <nav className={`${styles.navbar as string} theme-${theme as string}`}>
+      <SwitchThemeBtn />
+      <SignBtn />
+    </nav>
+  );
+};
